ad0111c94b58eccebbaa9cce838016eb60a4efe1
pauldennis2 <pauldennis.misc@gmail.com>
2018-09-17 16:02:04 -0400

    Spell feature WIP - still nothing broken. Added placeholder for some customization features.

--- src/main/java/paul/TextQuest/entities/Spell.java
+++ src/main/java/paul/TextQuest/entities/Spell.java
@@ -5,12 +5,12 @@
 package paul.TextQuest.entities;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.Scanner;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import paul.TextQuest.utils.StringUtils;
+import paul.TextQuest.enums.SpellTargetType;
+import paul.TextQuest.parsing.TextInterface;
 
 public class Spell {
 	
@@ -22,19 +22,79 @@ public class Spell {
 	private List<String> prereqs;
 	private List<String> requiredItems;
 	
-	private TargetType targetType;
+	private SpellTargetType targetType;
 	
 	public Spell () {
-		
+		actions = new ArrayList<>();
+		reagents = new ArrayList<>();
+		prereqs = new ArrayList<>();
+		requiredItems = new ArrayList<>();
 	}
 	
-	private static Spell jsonRestore(String spellJson) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(spellJson, Spell.class);
-    }
-	
 	public static void main(String[] args) throws IOException {
+		Spellbook spellbook = Spellbook.buildFromFile("content_files/game/spellbook.json");
 		System.out.println(Spellbook.buildFromFile("content_files/game/spellbook.json"));
+		Scanner inputScanner = new Scanner(System.in);
+		
+		while (true) {
+			String input = inputScanner.nextLine();
+			if (input.equals("")) {
+				break;
+			}
+			System.out.println("All spells with type " + input + ":");
+			List<Spell> spellsOfType = spellbook.getSpellsOfType(input);
+			spellsOfType.forEach(spell -> System.out.println("\t" + spell.getName()));
+		}
+		
+		inputScanner.close();
+	}
+	
+	
+	//Sandbox area for writing this code. Will probably end up in Hero
+	public static void castSpell (Spell spell, DungeonRoom location, Hero hero) {
+		TextInterface textOut = hero.getTextOut();
+		//Check prereqs
+		spell.getPrereqs().forEach(prereq -> {
+			List<String> knownSpells = hero.getSpellbook();
+			if (prereq.contains(" ")) {
+				String[] splits = prereq.split(" ");
+				int requiredLevel = Integer.parseInt(splits[1]);
+				String type = splits[0];
+				if (knownSpells.contains(type)) {
+					if (requiredLevel > 1) {
+						textOut.println("Your knowledge of " + prereq + " magic is not strong enough.");
+					}
+				} else {
+					textOut.println("You don't know the neccessary type of magic (" + prereq + ")");
+				}
+			} else {
+				if (!knownSpells.contains(prereq)) {
+					textOut.println("You don't know the neccessary type of magic (" + prereq + ")");
+					return;
+				}
+			}
+		});
+		
+		//Check required items
+		spell.getRequiredItems().forEach(itemName -> {
+			if (!hero.getBackpack().contains(itemName)) {
+				textOut.println("You are missing a required item.");
+				return;
+			}
+		});
+		
+		//Check and remove reagents
+		spell.getReagents().forEach(reagent -> {
+			if (!hero.getBackpack().contains(reagent)) {
+				textOut.println("You are missing a required reagent: " + reagent + ".");
+				return;
+			} else {
+				hero.getBackpack().remove(reagent);
+			}
+		});
+		
+		//Do spell actions
+		spell.getActions().forEach(location::doAction);
 	}
 
 	public String getName() {
@@ -77,11 +137,11 @@ public class Spell {
 		this.prereqs = prereqs;
 	}
 
-	public TargetType getTargetType() {
+	public SpellTargetType getTargetType() {
 		return targetType;
 	}
 
-	public void setTargetType(TargetType targetType) {
+	public void setTargetType(SpellTargetType targetType) {
 		this.targetType = targetType;
 	}
 	
@@ -98,40 +158,5 @@ public class Spell {
 	public String toString() {
 		return "Spell [name=" + name + ", key=" + key + ", actions=" + actions + ", reagents=" + reagents + ", prereqs="
 				+ prereqs + ", targetType=" + targetType + "]";
-	}
-	
-}
-
-class Spellbook {
-	Map<String, Spell> spellbook;
-	
-	public Spellbook () {
-		
-	}
-
-	public Map<String, Spell> getSpellbook() {
-		return spellbook;
-	}
-
-	public void setSpellbook(Map<String, Spell> spellbook) {
-		this.spellbook = spellbook;
-	}
-	
-	private static Spellbook jsonRestore(String spellbookJson) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(spellbookJson, Spellbook.class);
-    }
-	
-	public static Spellbook buildFromFile (String fileName) throws IOException {
-		return jsonRestore(StringUtils.readFile(fileName));
-	}
-	
-	public String toString () {
-		return spellbook.toString();
-	}
-	
-}
-
-enum TargetType {
-	SELF, ALL_ENEMIES, RANDOM_ENEMY, NONE;
+	}	
 }
---
+++ src/main/java/paul/TextQuest/enums/SpellTargetType.java
@@ -0,0 +1,9 @@
+/**
+ * @author Paul Dennis (pd236m)
+ * Sep 17, 2018
+ */
+package paul.TextQuest.enums;
+
+public enum SpellTargetType {
+	SELF, ALL_ENEMIES, RANDOM_ENEMY, NONE;
+}
